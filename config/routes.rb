bbfe13a7cce8913b21f17bd58106f7167b5a2c78
Timur Minulin <d@minulin.in>
2014-02-27 01:00:09 +0400

    omniauth fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Codeshare::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "user#omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
 
   resources :snippets
