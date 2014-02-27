024a339d490619290354237b9c7d28b2b6ae67d2
Timur Minulin <d@minulin.in>
2014-02-27 08:19:40 +0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Codeshare::Application.routes.draw do
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
   resources :users
 
   resources :snippets
@@ -9,7 +12,6 @@ Codeshare::Application.routes.draw do
   root to: 'users#welcome'
 
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
